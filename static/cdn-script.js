85659b7c793bf49214904448fe3d230aad8aa60a
egwimcodes <egwimcodes@gmail.com>
2025-01-14 12:41:12 +0100

    intellapen v1

---
+++ static/cdn-script.js
@@ -0,0 +1,41 @@
+    
+const config = {
+        theme: {
+        colors: {
+        blue: "#1fb6ff",
+    purple: "#7e5bef",
+    pink: {
+        100: "#ff80c3",
+    200: "#ff66b3",
+    300: "#ff49db",
+    400: "#ff33cc",
+    500: "#ff1acc",
+            },
+
+    orange: {
+        0: "#ff7849",
+
+    500: "#B44C2AFF",
+            },
+    green: "#13ce66",
+    yellow: "#ffc82c",
+    "gray-dark": "#273444",
+    gray: "#8492a6",
+    "gray-light": "#d3dce6",
+          },
+
+    fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+    serif: ["Merriweather", "serif"],
+          },
+    extend: {
+        spacing: {
+        "8xl": "96rem",
+    "9xl": "128rem",
+            },
+    borderRadius: {
+        "4xl": "2rem",
+            },
+          },
+        },
+      };
